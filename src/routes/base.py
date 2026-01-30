f47c986de9c9eae92db6bbe3124adf452c50337e
WA-on-test2 <marina.safwat2002@gmail.com>
2026-01-30 21:36:37 +0200

    setup API routing

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,17 +1,20 @@
-#base route
-#default route can be used for health check
-from fastapi import FastAPI, APIRouter
-import os
+from fastapi import FastAPI, APIRouter, Depends
+from helpers.config import get_settings, Settings
 
-base_router=APIRouter(
+base_router = APIRouter(
     prefix="/api/v1",
-    tags=["NLP","RAG"],
+    tags=["api_v1"],
 )
+
 @base_router.get("/")
-async def welcome():
-    app_name=os.getenv("APP_NAME")
-    app_version=os.getenv("APP_VERSION")
+async def welcome(app_settings: Settings = Depends(get_settings)):
+
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
+
+
     return {
-        "App_Name ": app_name,
-        "App_version":app_version,
+        "app_name": app_name,
+        "app_version": app_version,
+       
     }
